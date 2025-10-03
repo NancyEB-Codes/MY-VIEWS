a3adf27ee59635d0829582f4a5983dfc60714603
NancyEB-Codes <buatsinancyelikem@gmail.com>
2025-10-03 23:48:47 +0000

    Add files via upload

---
+++ A Data Exploration.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Welcome", layout="centered")
+
+st.title("👋 Welcome to My Streamlit App!")
+st.write("This is your homepage. You can build interactive dashboards, reports, and visualizations here.")
